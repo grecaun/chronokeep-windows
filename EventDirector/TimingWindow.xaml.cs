9ac863aad5f271a55835b5faec868afe3d4e418e
James Sentinella <sentinj@students.wwu.edu>
2018-09-28 21:36:57 -0700

    Started work on the Windows for timing with chips. Added RFIDSettings for modifying the settings of an RFID Ultra system. Added RFIDTime for setting the time on an RFID Ultra system. Added TimingRawReads for viewing the raw data from the timing system. Added TimingWindow for actually timing a race. This window is partially complete.

---
+++ EventDirector/TimingWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EventDirector
+{
+    /// <summary>
+    /// Interaction logic for TimingWindow.xaml
+    /// </summary>
+    public partial class TimingWindow : Window
+    {
+        public TimingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartTime_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
