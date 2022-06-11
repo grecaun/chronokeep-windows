1089422ddec67bb7278cb09eff839fce58095845
James Sentinella <james@sentinella.me>
2022-06-10 22:59:01 -0700

    Update checker now downloads the file instead of opening a web browser.

--- ChronoKeep/Updates/Check.cs
+++ ChronoKeep/Updates/Check.cs
@@ -3,10 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -86,8 +88,8 @@ namespace ChronoKeep.Updates
             }
             catch (Exception ex)
             {
-                Log.D("Updates.Check", ex.Message);
-                MessageBox.Show(ex.Message);
+                Log.E("Updates.Check", ex.Message);
+                MessageBox.Show("Unable to check for update.", "Error");
                 return;
             }
             GithubRelease latest_x86 = null;
@@ -129,12 +131,13 @@ namespace ChronoKeep.Updates
                 var selectedOption = MessageBox.Show("Download update?", "Update Found", MessageBoxButton.YesNo);
                 if (selectedOption == MessageBoxResult.Yes)
                 {
+                    Uri uri = null;
                     if (Is64Bit())
                     {
                         if (latest_x64 != null)
                         {
                             Log.D("Updates.Check", string.Format("Download URL (64 bit) - {0}", latest_x64.Assets[0].BrowserDownloadURL));
-                            System.Diagnostics.Process.Start(latest_x64.Assets[0].BrowserDownloadURL);
+                            uri = new Uri(latest_x64.Assets[0].BrowserDownloadURL);
                         }
                     }
                     else
@@ -142,7 +145,39 @@ namespace ChronoKeep.Updates
                         if (latest_x86 != null)
                         {
                             Log.D("Updates.Check", string.Format("Download URL (32 bit) - {0}", latest_x86.Assets[0].BrowserDownloadURL));
-                            System.Diagnostics.Process.Start(latest_x86.Assets[0].BrowserDownloadURL);
+                            uri = new Uri(latest_x86.Assets[0].BrowserDownloadURL);
+                        }
+                    }
+                    if (uri != null)
+                    {
+                        using (var client = GetHttpClient())
+                        {
+                            using (var file = new FileStream($"Chronokeep-{latest}-{(Is64Bit() ? "x64" : "x86")}.zip", FileMode.Create))
+                            {
+                                var progress = new Progress<double>();
+                                progress.ProgressChanged += (sender, value) =>
+                                {
+                                    if (value == 100)
+                                    {
+                                        Log.D("Updates.Check", "Download has finished!");
+                                        MessageBox.Show("Update downloaded!", "Success");
+                                    }
+                                    else
+                                    {
+                                        Log.D("Updates.Check", $"Download at {value}%");
+                                    }
+                                };
+                                var cancellationToken = new CancellationTokenSource();
+                                try
+                                {
+                                    await DownloadFileAsync(client, file, uri, progress, cancellationToken.Token);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Log.E("Updates.Check", $"Error downloading update. {ex.Message}");
+                                    MessageBox.Show("Unable to download update.", "Error");
+                                }
+                            }
                         }
                     }
                 }
@@ -192,6 +227,48 @@ namespace ChronoKeep.Updates
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.UserAgent.TryParseAdd("Chronokeep Desktop Application");
             return client;
+        }
+
+        private static async Task DownloadFileAsync(HttpClient client, Stream destination, Uri uri, IProgress<double> progress, CancellationToken token)
+        {
+            HttpResponseMessage response = await client.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead, token);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"The request returned with HTTP status code{response.StatusCode}");
+            }
+
+            long total = response.Content.Headers.ContentLength.HasValue ? response.Content.Headers.ContentLength.Value : -1L;
+            bool canReportProgress = total != -1 && progress != null;
+
+            using (var stream = await response.Content.ReadAsStreamAsync())
+            {
+                long totalRead = 0L;
+                byte[] buffer = new byte[8192];
+                bool isMoreToRead = true;
+                double lastReport = 0L;
+                do
+                {
+                    token.ThrowIfCancellationRequested();
+                    var read = await stream.ReadAsync(buffer, 0, buffer.Length, token);
+                    if (read == 0)
+                    {
+                        isMoreToRead = false;
+                    }
+                    else
+                    {
+                        await destination.WriteAsync(buffer, 0, read, token);
+                        totalRead += read;
+                        double report = Math.Truncate((totalRead * 1d) / (total * 1d) * 1000) / 10;
+                        if (canReportProgress && ((report > lastReport + 0.5) || report == 100))
+                        {
+                            progress.Report(report);
+                            lastReport = report;
+                        }
+                    }
+                } while (isMoreToRead);
+            }
+
         }
     }
 }
