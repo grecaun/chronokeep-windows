e8dba3d3763a6d5a3d4b952386ef04bcc9b0d69a
James Sentinella <james@sentinella.me>
2022-06-11 17:09:00 -0700

    Added a new window to download and eventually install new version over top of old version of app.

--- ChronoKeep/Updates/Check.cs
+++ ChronoKeep/Updates/Check.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,7 +15,7 @@ using System.Windows;
 
 namespace ChronoKeep.Updates
 {
-    internal class Version
+    public class Version
     {
         public int major;
         public int minor;
@@ -56,7 +57,13 @@ namespace ChronoKeep.Updates
         }
     }
 
-    internal class Check
+    public class Release
+    {
+        internal GithubRelease x86 = null;
+        internal GithubRelease x64 = null;
+    }
+
+    public class Check
     {
         private static string RepoURL = "https://api.github.com/repos/grecaun/chronokeep-windows/releases";
 
@@ -92,101 +99,44 @@ namespace ChronoKeep.Updates
                 MessageBox.Show("Unable to check for update.", "Error");
                 return;
             }
-            GithubRelease latest_x86 = null;
-            GithubRelease latest_x64 = null;
-            Version latest = new Version();
+            Release latestRelease = new Release();
+            Version latestVersion = new Version();
             foreach (GithubRelease release in releases)
             {
-                Version rel = new Version();
+                Version releaseVersion = new Version();
                 version = release.Name.Split('.');
                 if (version.Length == 4)
                 {
                     version[0] = version[0].Replace('v', '0');
                     version[2] = version[2].Split('-')[0];
-                    rel.major = int.Parse(version[0]);
-                    rel.minor = int.Parse(version[1]);
-                    rel.patch = int.Parse(version[2]);
+                    releaseVersion.major = int.Parse(version[0]);
+                    releaseVersion.minor = int.Parse(version[1]);
+                    releaseVersion.patch = int.Parse(version[2]);
                 }
                 // Check for major version updates
                 // Then minor version updates
                 // patches
                 // check for equal versions because we store both x86 and x64 versions
-                if (rel.Newer(latest) || rel.Equal(latest))
+                if (releaseVersion.Newer(latestVersion) || releaseVersion.Equal(latestVersion))
                 {
                     if (version[3].Equals("x86", StringComparison.OrdinalIgnoreCase))
                     {
-                        latest_x86 = release;
+                        latestRelease.x86 = release;
                     }
                     else if (version[3].Equals("x64", StringComparison.OrdinalIgnoreCase))
                     {
-                        latest_x64 = release;
+                        latestRelease.x64 = release;
                     }
-                    latest.Set(rel);
+                    latestVersion.Set(releaseVersion);
                 }
             }
-            Log.D("Updates.Check", string.Format("Latest version is {0}", latest.ToString()));
-            if (latest.Newer(current))
+            Log.D("Updates.Check", string.Format("Latest version is {0}", latestVersion.ToString()));
+            if (latestVersion.Newer(current))
             {
                 Log.D("Updates.Check", "Newer version found.");
-                var selectedOption = MessageBox.Show("Download update?", "Update Found", MessageBoxButton.YesNo);
-                if (selectedOption == MessageBoxResult.Yes)
-                {
-                    Uri uri = null;
-                    if (Is64Bit())
-                    {
-                        if (latest_x64 != null)
-                        {
-                            Log.D("Updates.Check", string.Format("Download URL (64 bit) - {0}", latest_x64.Assets[0].BrowserDownloadURL));
-                            uri = new Uri(latest_x64.Assets[0].BrowserDownloadURL);
-                        }
-                    }
-                    else
-                    {
-                        if (latest_x86 != null)
-                        {
-                            Log.D("Updates.Check", string.Format("Download URL (32 bit) - {0}", latest_x86.Assets[0].BrowserDownloadURL));
-                            uri = new Uri(latest_x86.Assets[0].BrowserDownloadURL);
-                        }
-                    }
-                    if (uri != null)
-                    {
-                        using (var client = GetHttpClient())
-                        {
-                            using (var file = new FileStream($"Chronokeep-{latest}-{(Is64Bit() ? "x64" : "x86")}.zip", FileMode.Create))
-                            {
-                                var progress = new Progress<double>();
-                                progress.ProgressChanged += (sender, value) =>
-                                {
-                                    if (value == 100)
-                                    {
-                                        Log.D("Updates.Check", "Download has finished!");
-                                        MessageBox.Show("Update downloaded!", "Success");
-                                    }
-                                    else
-                                    {
-                                        Log.D("Updates.Check", $"Download at {value}%");
-                                    }
-                                };
-                                var cancellationToken = new CancellationTokenSource();
-                                try
-                                {
-                                    await DownloadFileAsync(client, file, uri, progress, cancellationToken.Token);
-                                }
-                                catch (Exception ex)
-                                {
-                                    Log.E("Updates.Check", $"Error downloading update. {ex.Message}");
-                                    MessageBox.Show("Unable to download update.", "Error");
-                                }
-                            }
-                        }
-                    }
-                }
+                DownloadWindow downloadWindow = new DownloadWindow(latestRelease, latestVersion);
+                downloadWindow.Show();
             }
-        }
-
-        private static bool Is64Bit()
-        {
-            return IntPtr.Size == 8;
         }
 
         private static async Task<List<GithubRelease>> GetReleases()
@@ -227,48 +177,6 @@ namespace ChronoKeep.Updates
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             client.DefaultRequestHeaders.UserAgent.TryParseAdd("Chronokeep Desktop Application");
             return client;
-        }
-
-        private static async Task DownloadFileAsync(HttpClient client, Stream destination, Uri uri, IProgress<double> progress, CancellationToken token)
-        {
-            HttpResponseMessage response = await client.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead, token);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                throw new Exception($"The request returned with HTTP status code{response.StatusCode}");
-            }
-
-            long total = response.Content.Headers.ContentLength.HasValue ? response.Content.Headers.ContentLength.Value : -1L;
-            bool canReportProgress = total != -1 && progress != null;
-
-            using (var stream = await response.Content.ReadAsStreamAsync())
-            {
-                long totalRead = 0L;
-                byte[] buffer = new byte[8192];
-                bool isMoreToRead = true;
-                double lastReport = 0L;
-                do
-                {
-                    token.ThrowIfCancellationRequested();
-                    var read = await stream.ReadAsync(buffer, 0, buffer.Length, token);
-                    if (read == 0)
-                    {
-                        isMoreToRead = false;
-                    }
-                    else
-                    {
-                        await destination.WriteAsync(buffer, 0, read, token);
-                        totalRead += read;
-                        double report = Math.Truncate((totalRead * 1d) / (total * 1d) * 1000) / 10;
-                        if (canReportProgress && ((report > lastReport + 0.5) || report == 100))
-                        {
-                            progress.Report(report);
-                            lastReport = report;
-                        }
-                    }
-                } while (isMoreToRead);
-            }
-
         }
     }
 }
---
+++ ChronoKeep/Updates/DownloadWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChronoKeep.Updates
+{
+    /// <summary>
+    /// Interaction logic for DownloadWindow.xaml
+    /// </summary>
+    public partial class DownloadWindow : Window
+    {
+        private string uri;
+        private string download_uri;
+        private string version;
+
+        private CancellationTokenSource cancellationToken;
+
+        public DownloadWindow(Release release, Version version)
+        {
+            InitializeComponent();
+            DownloadProgress.Visibility = Visibility.Collapsed;
+            this.version = version.ToString();
+            if (Is64Bit())
+            {
+                if (release.x64 != null)
+                {
+                    Log.D("Updates.Check", string.Format("Download URL (64 bit) - {0}", release.x64.Assets[0].BrowserDownloadURL));
+                    uri = release.x64.Assets[0].BrowserDownloadURL;
+                }
+            }
+            else
+            {
+                if (release.x86 != null)
+                {
+                    Log.D("Updates.Check", string.Format("Download URL (32 bit) - {0}", release.x86.Assets[0].BrowserDownloadURL));
+                    uri = release.x86.Assets[0].BrowserDownloadURL;
+                }
+            }
+            download_uri = $"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\\Chronokeep-{version}-{(Is64Bit() ? "x64" : "x86")}.zip";
+        }
+
+        private static bool Is64Bit()
+        {
+            return IntPtr.Size == 8;
+        }
+
+        private static HttpClient GetHttpClient()
+        {
+            var handler = new WinHttpHandler();
+            var client = new HttpClient(handler);
+            client.Timeout = TimeSpan.FromSeconds(2);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.UserAgent.TryParseAdd("Chronokeep Desktop Application");
+            return client;
+        }
+        private static async Task DownloadFileAsync(HttpClient client, Stream destination, string uri, IProgress<double> progress, CancellationToken token)
+        {
+            HttpResponseMessage response = await client.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead, token);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"The request returned with HTTP status code{response.StatusCode}");
+            }
+
+            long total = response.Content.Headers.ContentLength.HasValue ? response.Content.Headers.ContentLength.Value : -1L;
+            bool canReportProgress = total != -1 && progress != null;
+
+            using (var stream = await response.Content.ReadAsStreamAsync())
+            {
+                long totalRead = 0L;
+                byte[] buffer = new byte[8192];
+                bool isMoreToRead = true;
+                double lastReport = 0L;
+                do
+                {
+                    token.ThrowIfCancellationRequested();
+                    var read = await stream.ReadAsync(buffer, 0, buffer.Length, token);
+                    if (read == 0)
+                    {
+                        isMoreToRead = false;
+                    }
+                    else
+                    {
+                        await destination.WriteAsync(buffer, 0, read, token);
+                        totalRead += read;
+                        double report = Math.Truncate((totalRead * 1d) / (total * 1d) * 1000) / 10;
+                        if (canReportProgress && ((report > lastReport + 0.5) || report == 100))
+                        {
+                            progress.Report(report);
+                            lastReport = report;
+                        }
+                    }
+                } while (isMoreToRead);
+            }
+
+        }
+
+        private async void InstallButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (((string)InstallButton.Content).Equals("Download", StringComparison.OrdinalIgnoreCase))
+            {
+                Log.D("Updates.DownloadWindow", $"Download clicked. Downloading to {download_uri}");
+                DownloadProgress.Visibility = Visibility.Visible;
+                DownloadLabel.Content = $"Downloading {version}";
+                InstallButton.Content = "Install";
+                InstallButton.IsEnabled = false;
+                using (var client = GetHttpClient())
+                {
+                    using (var file = new FileStream(download_uri, FileMode.Create))
+                    {
+                        var progress = new Progress<double>();
+                        progress.ProgressChanged += (s, value) =>
+                        {
+                            Log.D("Updates.Check", $"Download at {value}%");
+                            DownloadProgress.Value = value;
+                        };
+                        cancellationToken = new CancellationTokenSource();
+                        try
+                        {
+                            await DownloadFileAsync(client, file, uri, progress, cancellationToken.Token);
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.E("Updates.Check", $"Error downloading update. {ex.Message}");
+                            MessageBox.Show("Unable to download update.", "Error");
+                        }
+                    }
+                }
+                InstallButton.IsEnabled = true;
+            }
+            else if (((string)InstallButton.Content).Equals("Install", StringComparison.OrdinalIgnoreCase))
+            {
+                Log.D("Updates.DownloadWindow", "Install clicked.");
+            }
+            else
+            {
+                Log.D("Updates.DownloadWindow", "Something went wrong and button text was not valid.");
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Log.D("Updates.DownloadWindow", "Cancel clicked.");
+            cancellationToken.Cancel();
+            this.Close();
+        }
+    }
+}
