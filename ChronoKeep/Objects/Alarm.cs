25c18eb550b5b4051d0ae8dde27910899769240f
James Sentinella <james@sentinella.me>
2023-12-17 20:16:18 -0800

    Config file now properly declares alarm sounds need to be copied. Made alarms comparable. They will sort by bib/chip. Alarms page now does things. No database use for the alarms currently.

--- ChronoKeep/Objects/Alarm.cs
+++ ChronoKeep/Objects/Alarm.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace Chronokeep.Objects
 {
-    internal class Alarm
+    internal class Alarm : IEquatable<Alarm>, IComparable<Alarm>
     {
+        public int Identifier { get; set; }
         public int Bib { get; set; } = -1;
         public string Chip { get; set; } = "";
         // This is the number of times to alert the user.
@@ -17,5 +18,21 @@ namespace Chronokeep.Objects
         public bool Enabled { get; set; } = false;
         // Any number not assigned to a sound is assumed to be the default.
         public int AlarmSound { get; set; } = 0;
+
+        public int CompareTo(Alarm other)
+        {
+            if (other == null) return 1;
+            if (this.Bib == other.Bib)
+            {
+                return this.Chip.CompareTo(other.Chip);
+            }
+            return this.Bib.CompareTo(other.Bib);
+        }
+
+        public bool Equals(Alarm other)
+        {
+            if (other == null) return false;
+            return this.Identifier == other.Identifier;
+        }
     }
 }
--- ChronoKeep/UI/Timing/AlarmsPage.xaml.cs
+++ ChronoKeep/UI/Timing/AlarmsPage.xaml.cs
@@ -20,18 +20,19 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using Wpf.Ui.Controls;
 
 namespace Chronokeep.UI.Timing
 {
     /// <summary>
-    /// Interaction logic for AwardPage.xaml
+    /// Interaction logic for AlarmPage.xaml
     /// </summary>
     public partial class AlarmsPage : ISubPage
     {
         IDBInterface database;
         TimingPage parent;
         Event theEvent;
+
+        List<Alarm> alarms = new List<Alarm>();
 
         public AlarmsPage(TimingPage parent, IDBInterface database)
         {
@@ -41,9 +42,11 @@ namespace Chronokeep.UI.Timing
             theEvent = database.GetCurrentEvent();
             if (theEvent == null || theEvent.Identifier < 0)
             {
-                Log.E("UI.Timing.AwardPage", "Something went wrong and no proper event was returned.");
+                Log.E("UI.Timing.AlarmsPage", "Something went wrong and no proper event was returned.");
                 return;
             }
+            // get alarms from the database
+            // alarms = database.GetAlarms(theEvent.Identifier);
             UpdateView();
         }
         
@@ -51,7 +54,7 @@ namespace Chronokeep.UI.Timing
 
         private void DoneButton_Click(object sender, RoutedEventArgs e)
         {
-            Log.D("UI.Timing.AwardPage", "Done clicked.");
+            Log.D("UI.Timing.AlarmsPage", "Done clicked.");
             parent.LoadMainDisplay();
         }
 
@@ -63,15 +66,280 @@ namespace Chronokeep.UI.Timing
 
         public void UpdateView()
         {
+            Log.D("UI.Timing.AlarmsPage", "Updating View.");
+            if (theEvent == null || theEvent.Identifier < 0)
+            {
+                return;
+            }
+            AlarmsBox.Items.Clear();
+            // alarms = database.GetAlarms(theEvent.Identifier);
+            alarms.Sort();
+            foreach (Alarm alarm in alarms)
+            {
+                AlarmsBox.Items.Add(new AnAlarmItem(this, alarm));
+            }
         }
 
-        public void Closing() { }
+        private void SaveAlarms()
+        {
+            Log.D("UI.Timing.AlarmsPage", "Saving Alarms.");
+            alarms.Clear();
+            foreach (AnAlarmItem alarm in AlarmsBox.Items)
+            {
+                alarms.Add(alarm.GetUpdatedAlarm());
+            }
+            /*
+             * database.SaveAlarms(alarms);
+             */
+        }
+
+        public void Closing()
+        {
+            Log.D("UI.Timing.AlarmsPage", "Closing Page.");
+            if (database.GetAppSetting(Constants.Settings.UPDATE_ON_PAGE_CHANGE).Value == Constants.Settings.SETTING_TRUE)
+            {
+                SaveAlarms();
+            }
+        }
 
         public void Keyboard_Ctrl_A() { }
 
-        public void Keyboard_Ctrl_S() { }
+        public void Keyboard_Ctrl_S()
+        {
+            Log.D("UI.Timing.AlarmsPage", "Ctrl+S pressed.");
+            SaveAlarms();
+        }
 
-        public void Keyboard_Ctrl_Z() { }
+        public void Keyboard_Ctrl_Z()
+        {
+            Log.D("UI.Timing.AlarmsPage", "Ctrl+Z pressed.");
+            UpdateView();
+        }
+
+        private void AlarmsBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (Utils.GetScrollViewer(sender as DependencyObject) is ScrollViewer scrollViewer)
+            {
+                if (e.Delta < 0)
+                {
+                    scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset + 35);
+                }
+                else if (e.Delta > 0)
+                {
+                    scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - 35);
+                }
+            }
+        }
+
+        private void RemoveAlarm(AnAlarmItem alarm)
+        {
+            AlarmsBox.Items.Remove(alarm);
+            SaveAlarms();
+            UpdateView();
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            Log.D("UI.Timing.AlarmsPage", "Add button clicked.");
+            AlarmsBox.Items.Add(new AnAlarmItem(this, new Alarm()));
+            SaveAlarms();
+            UpdateView();
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            Log.D("UI.Timing.AlarmsPage", "Save button clicked.");
+            SaveAlarms();
+            UpdateView();
+        }
+
+        private class AnAlarmItem : ListBoxItem
+        {
+            public TextBox BibBox;
+            public TextBox ChipBox;
+            public TextBox AlertCountBox;
+            public Label AlertedCountLabel;
+            public ComboBox AlarmSoundBox;
+            public System.Windows.Controls.CheckBox EnabledBox;
+            public Button RemoveButton;
+
+            readonly AlarmsPage page;
+            private Alarm theAlarm;
+
+            // Allow only numbers in our bib/alert count boxes.
+            private readonly Regex allowedChars = new Regex("[^0-9]");
+
+            public AnAlarmItem(AlarmsPage page, Alarm alarm)
+            {
+                this.page = page;
+                this.theAlarm = alarm;
+                Grid theGrid = new Grid()
+                {
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                };
+                this.Content = theGrid;
+                this.IsTabStop = false;
+
+                theGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(100) });
+                theGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(100) });
+                theGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(100) });
+                theGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(100) });
+                theGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(175) });
+                theGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(52) });
+                theGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(45) });
+                BibBox = new TextBox()
+                {
+                    Text = alarm.Bib < 0 ? "" : alarm.Bib.ToString(),
+                    FontSize = 16,
+                    Margin = new Thickness(2),
+                    VerticalAlignment = VerticalAlignment.Center,
+                };
+                BibBox.GotFocus += new RoutedEventHandler(SelectAll);
+                BibBox.PreviewTextInput += new TextCompositionEventHandler(NumberValidation);
+                theGrid.Children.Add(BibBox);
+                Grid.SetColumn(BibBox, 0);
+                ChipBox = new TextBox()
+                {
+                    Text = alarm.Chip,
+                    FontSize = 16,
+                    Margin = new Thickness(2),
+                    VerticalAlignment = VerticalAlignment.Center,
+                };
+                ChipBox.GotFocus += new RoutedEventHandler(SelectAll);
+                theGrid.Children.Add(ChipBox);
+                Grid.SetColumn(ChipBox, 1);
+                AlertCountBox = new TextBox()
+                {
+                    Text = alarm.AlertCount < 1 ? "1" : alarm.AlertCount.ToString(),
+                    FontSize = 16,
+                    Margin = new Thickness(2),
+                    VerticalAlignment = VerticalAlignment.Center,
+                };
+                AlertCountBox.GotFocus += new RoutedEventHandler(SelectAll);
+                AlertCountBox.PreviewTextInput += new TextCompositionEventHandler(NumberValidation);
+                theGrid.Children.Add(AlertCountBox);
+                Grid.SetColumn(AlertCountBox, 2);
+                AlertedCountLabel = new Label()
+                {
+                    Content = alarm.AlertedCount.ToString(),
+                    FontSize = 16,
+                    Margin = new Thickness(2),
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                };
+                theGrid.Children.Add(AlertedCountLabel);
+                Grid.SetColumn(AlertedCountLabel, 3);
+                AlarmSoundBox = new ComboBox()
+                {
+                    FontSize = 16,
+                    Margin = new Thickness(2),
+                    VerticalAlignment = VerticalAlignment.Center,
+                };
+                AlarmSoundBox.Items.Add(
+                    new ComboBoxItem
+                    {
+                        Content = "Default",
+                        Uid = "0",
+                    }
+                    );
+                AlarmSoundBox.Items.Add(
+                    new ComboBoxItem
+                    {
+                        Content = "Sound 1",
+                        Uid = "1",
+                    }
+                    );
+                AlarmSoundBox.Items.Add(
+                    new ComboBoxItem
+                    {
+                        Content = "Sound 2",
+                        Uid = "2",
+                    }
+                    );
+                AlarmSoundBox.Items.Add(
+                    new ComboBoxItem
+                    {
+                        Content = "Sound 3",
+                        Uid = "3",
+                    }
+                    );
+                AlarmSoundBox.Items.Add(
+                    new ComboBoxItem
+                    {
+                        Content = "Sound 4",
+                        Uid = "4",
+                    }
+                    );
+                AlarmSoundBox.Items.Add(
+                    new ComboBoxItem
+                    {
+                        Content = "Sound 5",
+                        Uid = "5",
+                    }
+                    );
+                AlarmSoundBox.SelectedIndex = alarm.AlarmSound;
+                theGrid.Children.Add(AlarmSoundBox);
+                Grid.SetColumn(AlarmSoundBox, 4);
+                EnabledBox = new System.Windows.Controls.CheckBox()
+                {
+                    IsChecked = alarm.Enabled,
+                    Margin = new Thickness(2),
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                };
+                theGrid.Children.Add(EnabledBox);
+                Grid.SetColumn(EnabledBox, 5);
+                RemoveButton = new Button()
+                {
+                    Content = "X",
+                    FontSize = 16,
+                    Width = 35,
+                    Height = 35,
+                    Margin = new Thickness(2),
+                    VerticalAlignment = VerticalAlignment.Center,
+                };
+                RemoveButton.Click += new RoutedEventHandler(Remove_Click);
+                theGrid.Children.Add(RemoveButton);
+                Grid.SetColumn(RemoveButton, 6);
+            }
+
+            private void Remove_Click(object sender, RoutedEventArgs e)
+            {
+                Log.D("UI.Timing.AlarmsPage", "Removing alarm.");
+                page.RemoveAlarm(this);
+            }
+
+            private void NumberValidation(object sender, TextCompositionEventArgs e)
+            {
+                e.Handled = allowedChars.IsMatch(e.Text);
+            }
+
+            private void SelectAll(object sender, RoutedEventArgs e)
+            {
+                TextBox src = (TextBox)e.OriginalSource;
+                src.SelectAll();
+            }
+
+            public Alarm GetUpdatedAlarm()
+            {
+                int tmpBib, tmpAlertCount;
+                if (int.TryParse(BibBox.Text, out tmpBib) == false)
+                {
+                    tmpBib = -1;
+                }
+                if (int.TryParse(AlertCountBox.Text, out tmpAlertCount) == false)
+                {
+                    tmpAlertCount = 1;
+                }
+                theAlarm.Bib = tmpBib;
+                theAlarm.Chip = ChipBox.Text;
+                theAlarm.AlertCount = tmpAlertCount;
+                theAlarm.Enabled = EnabledBox.IsChecked == true;
+                theAlarm.AlarmSound = AlarmSoundBox.SelectedIndex;
+                return theAlarm;
+            }
+        }
     }
 }
  
