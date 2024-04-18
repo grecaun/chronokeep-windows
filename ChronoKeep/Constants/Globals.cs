6856d7bd4ee8ca04d8f7074b0432b451833243ee
James Sentinella <james@sentinella.me>
2024-04-17 18:19:59 -0700

    Adding placeholders for keeping track of information we will need to send SMS/Email alerts to runners.

---
+++ ChronoKeep/Constants/Globals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chronokeep.Constants
+{
+    public class Globals
+    {
+        // keep track of TWILIO credentials
+        // keep track of banned phones
+        // keep track of banned emails
+    }
+}
