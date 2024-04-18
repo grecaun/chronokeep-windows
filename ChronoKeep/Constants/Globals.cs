ecb726279fa0aaa75ad3e6b2011b5e9a5355f358
James Sentinella <james@sentinella.me>
2024-04-18 15:17:04 -0700

    More boilerplate for Twilio SMS notifications.

--- ChronoKeep/Constants/Globals.cs
+++ ChronoKeep/Constants/Globals.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chronokeep.Objects.Notifications;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,10 @@ namespace Chronokeep.Constants
     public class Globals
     {
         // keep track of TWILIO credentials
+        public static Twilio TwilioCredentials = new();
         // keep track of banned phones
+        public static List<string> BannedPhones = new List<string>();
         // keep track of banned emails
+        public static List<string> BannedEmails = new List<string>();
     }
 }
