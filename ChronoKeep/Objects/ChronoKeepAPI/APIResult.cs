924b804802722c02a0fb923815a2da9db55204a3
James Sentinella <james@sentinella.me>
2025-04-07 11:38:13 -0700

    Fixed an issue where a person could run two events in a race but their results would be mixed together when uploaded to the Chronokeep results api.

--- ChronoKeep/Objects/ChronoKeepAPI/APIResult.cs
+++ ChronoKeep/Objects/ChronoKeepAPI/APIResult.cs
@@ -7,7 +7,7 @@ namespace Chronokeep.Objects.API
     {
         public APIResult(Event theEvent, TimeResult result, DateTime start, string unique_pad)
         {
-            this.PersonId = string.Format("{0}-{1}", result.ParticipantId, unique_pad);
+            this.PersonId = string.Format("{0}-{1}", result.EventSpecificId, unique_pad);
             this.Bib = result.Bib.ToString();
             this.First = result.Anonymous ? "" : result.First;
             this.Last = result.Anonymous ? "" : result.Last;
